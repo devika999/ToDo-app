9f448273ba800c01a19df1b06a1f85e8e1c4ccf0
devika999 <85698944+devika999@users.noreply.github.com>
2023-02-16 01:26:18 +0530

    Delete mytravelapp directory

--- mytravelapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from mytravelapp.models import Place, Team
-
-admin.site.register(Place)
-admin.site.register(Team)
--- mytravelapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from .models import Place, Team
-
-def demo(request):
-    obj = Place.objects.all()
-    obj1 = Team.objects.all()
-    return render(request,"index.html",{'result':obj,'result1':obj1})
